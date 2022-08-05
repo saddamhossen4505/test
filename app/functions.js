afefdb1529646c7717df1bd73a3e153884a45a4b
saddamhossen4505 <seoexpartsaddamhossen@gmail.com>
2022-08-05 18:23:04 +0600

    upload files for test

---
+++ app/functions.js
@@ -0,0 +1,10 @@
+
+
+// ageCal functions
+
+const ageCal = (name, age) => {
+    return `Hi ${name}, You are ${age} age years old.`;
+};
+
+
+module.exports = ageCal;
---
+++ index.js
@@ -0,0 +1,5 @@
+
+
+const ageCal = require('./app/functions.js');
+
+console.log(ageCal('Saddam', 28));
